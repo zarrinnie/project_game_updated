e577eafa5ac08bfeebbef636e1e0d9c720747d7b
YvesStraten <yves.straten@gmail.com>
2024-08-19 10:21:10 +0800

    Level_UI: Added finished level star animation

--- Assets/Scripts/FinishedLevelManager/FinishedLevelShowState.cs
+++ Assets/Scripts/FinishedLevelManager/FinishedLevelShowState.cs
@@ -8,10 +8,18 @@ public class FinishedLevelShowState : State<FinishedLevelManager>
     private SaveDataManager saveDataManager;
     [SerializeField]
     private DrawerManager drawerManager;
+    [SerializeField]
+    private GameObject[] stars;
     public override void EnterState(FinishedLevelManager finishedLevelManager)
     {
         saveDataManager = GameObject.Find("SaveDataManager").GetComponent<SaveDataManager>();
         finishedLevelManager.canvas.enabled = true;
+
+        foreach(GameObject star in stars){
+            star.SetActive(true);
+        }
+
+
         saveDataManager.saving.drawer = drawerManager;
         saveDataManager.SwitchState(saveDataManager.saving);
     }
